cd8101ae86d28c7c0459ae26adb3a45e7147520d
jyzackoh <zackkohjunyang@gmail.com>
2015-03-23 19:08:40 +0800

    Modified SimulatorClient
    Modified SimulatorClient to repeatedly try to connect to the server in
    event of disconnection.

--- src/network/SimulatorClient.java
+++ src/network/SimulatorClient.java
@@ -11,39 +11,59 @@ public class SimulatorClient implements Runnable {
 
 	private String host;
 	private int port;
+	private ObjectOutputStream outToServer;
+	private ObjectInputStream inFromServer;
 	
 	public SimulatorClient(String host, int port) {
 		this.host = host;
 		this.port = port;
 	}
 	
-	@SuppressWarnings("resource")
 	@Override
 	public void run() {
-		try {
-			Socket clientSocket = new Socket(this.host, this.port);
-			ObjectOutputStream outToServer = new ObjectOutputStream(clientSocket.getOutputStream());
-			ObjectInputStream inFromServer = new ObjectInputStream(clientSocket.getInputStream());
-
-			System.out.println("done init!");
-			
-			while (true) {
-				//Request for a new job
-				Integer request = new Integer(0);
-				outToServer.writeObject(request);
-				double[] vector = (double[]) inFromServer.readObject();
-				while (vector == null) {
-					outToServer.writeObject(request);
-					vector = (double[]) inFromServer.readObject();
-				}
+		while (true) {
+			try {
+				connectToHost();
+				System.out.println("done init!");
 				
-				System.out.println("Got New Job");
-				outToServer.writeObject(executeGame(vector));
-				System.out.println("Done Job");
+				while (true) {
+					//Request for a new job
+					Integer request = new Integer(0);
+					outToServer.writeObject(request);
+					double[] vector = (double[]) inFromServer.readObject();
+					while (vector == null) {
+						outToServer.writeObject(request);
+						vector = (double[]) inFromServer.readObject();
+					}
+					
+					System.out.println("Got New Job");
+					outToServer.writeObject(executeGame(vector));
+					System.out.println("Done Job");
+				}
+			} catch (Exception closedSocket) {
+				System.out.println("closed!");
+				closedSocket.printStackTrace();
 			}
-		} catch (Exception closedSocket) {
-			System.out.println("closed!");
-			closedSocket.printStackTrace();
+		}
+	}
+
+	private void connectToHost() {
+		Socket clientSocket;
+		int timeoff = 1;
+		while (true) {
+			try {
+				Thread.sleep(timeoff*500);
+				clientSocket = new Socket(this.host, this.port);
+				this.outToServer = new ObjectOutputStream(clientSocket.getOutputStream());
+				this.inFromServer = new ObjectInputStream(clientSocket.getInputStream());
+				break;
+			} catch (Exception e) {
+				System.out.println("SUMTIN WONG");
+				e.printStackTrace();
+			}
+			if (timeoff < 8) {
+				timeoff *= 2;	
+			}
 		}
 	}
 
